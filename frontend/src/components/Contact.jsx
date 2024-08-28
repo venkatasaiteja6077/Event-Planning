04acd81c3686b6aa9ad9ff6b281a5bfbc8a4de5a
venkatasaiteja6077 <124488925+venkatasaiteja6077@users.noreply.github.com>
2024-08-28 18:23:51 +0530

    Update Contact.jsx

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     e.preventDefault();
     await axios
       .post(
-        "http://localhost:4000/api/v1/message/send",
+        "https://event-planning-h2dr.onrender.com/message/send",
         {
           name,
           email,
